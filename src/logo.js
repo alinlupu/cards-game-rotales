ff3d75a7198453bc32c7c77710726b22a9bcca0d
Alin Lupu <alinlupu.dev@gmail.com>
2022-11-18 19:49:31 +0200

    Add logo, backside card

---
+++ src/logo.js
@@ -0,0 +1,17 @@
+function Logo( _el ) {
+	this._el = _el;
+	this.move = function() {
+		let _e = this._el;
+		_e.style.top = "0px";
+		_e.style.width = "70px"; 
+		setTimeout( function() {
+			_e.style.opacity = "0";
+		}, 1000);
+		setTimeout( function() {
+			_e.parentElement.style.justifyContent = "flex-start";
+			_e.style.opacity = "1";
+		}, 2000);
+	}
+}
+
+export default Logo;
