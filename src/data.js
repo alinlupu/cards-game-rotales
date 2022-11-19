e6558deecc526c558b5816793d79361d9105d3b8
Alin Lupu <alinlupu.dev@gmail.com>
2022-11-19 11:23:27 +0200

    Add audio tracks, intro texts, fix randomization of cards

--- src/data.js
+++ src/data.js
@@ -1,92 +1,135 @@
 const data = {
-  "1": {
-	"name": "Baba Dochia",
-    "path": "assets/BABA_DOCHIA.png"
-  },
-  "2": {
-	"name": "Calul Năzdrăvan",
-    "path": "assets/CALUL_NAZDRAVAN.png"
-  },
-  "3": {
-	"name": "Capra",
-    "path": "assets/CAPRA.png"
-  },
-  "4": {
-	"name": "Cerbul",
-    "path": "assets/CERBUL.png"
-  },
-  "5": {
-	"name": "Drăgaica",
-    "path": "assets/DRAGAICA.png"
-  },
-  "6": {
-	"name": "Greuceanu",
-    "path": "assets/GREUCEANU.png"
-  },
-  "7": {
-	"name": "Luceafărul",
-    "path": "assets/LUCEAFARUL.png"
-  },
-  "8": {
-	"name": "Lupul",
-    "path": "assets/LUPUL.png"
-  },
-  "9": {
-	"name": "Mama Pădurii",
-    "path": "assets/MAMA_PADURII.png"
-  },
-  "10": {
-	"name": "Martolea",
-    "path": "assets/MARTOLEA.png"
-  },
-  "11": {
-	"name": "Miorița",
-    "path": "assets/MIORITA.png"
-  },
-  "12": {
-	"name": "Moș Basca",
-    "path": "assets/MOS_BASCA.png"
-  },
-  "13": {
-	"name": "Pasărea Unicorn",
-    "path": "assets/PASAREA_UNICORN.png"
-  },
-  "14": {
-	"name": "Samca",
-    "path": "assets/SAMCA.png"
-  },
-  "15": {
-	"name": "Solomonar",
-    "path": "assets/SOLOMONAR.png"
-  },
-  "16": {
-	"name": "Ștefan cel Mare",
-    "path": "assets/STEFAN_DEL_MARE.png"
-  },
-  "17": {
-	"name": "Tatăl Pădurii",
-    "path": "assets/TATAL_PADURII.png"
-  },
-  "18": {
-	"name": "Urși",
-    "path": "assets/URSI.png"
-  },
-  "19": {
-	"name": "Verde Împărat",
-    "path": "assets/VERDE_IMPARAT.png"
-  },
-  "20": {
-	"name": "Zburătorul",
-    "path": "assets/ZBURATOR.png"
-  },
-  "21": {
-	"name": "Zmeul",
-    "path": "assets/ZMEUL.png"
-  },
-  "22": {
-	"name": "Zorilă",
-    "path": "assets/ZORILA.png"
-  }
+	"1": {
+		"name": "Baba Dochia",
+		"path": "assets/BABA_DOCHIA.png",
+		"audio_path": "assets/baba-dochia.m4a"
+	},
+	"2": {
+		"name": "Calul Năzdrăvan",
+		"path": "assets/CALUL_NAZDRAVAN.png",
+		"audio_path": "assets/calul.m4a"
+	},
+	"3": {
+		"name": "Capra",
+		"path": "assets/CAPRA.png",
+		"audio_path": "assets/capra.m4a"
+	},
+	"4": {
+		"name": "Samca",
+		"path": "assets/SAMCA.png",
+		"audio_path": "assets/samca.m4a"
+	},
+	"5": {
+		"name": "Solomonar",
+		"path": "assets/SOLOMONAR.png",
+		"audio_path": "assets/solomonar.m4a"
+	},
+	"6": {
+		"name": "Ștefan cel Mare",
+		"path": "assets/STEFAN_DEL_MARE.png",
+		"audio_path": "assets/stefan-cel-mare.m4a"
+	},
+	"7": {
+		"name": "Urși",
+		"path": "assets/URSI.png",
+		"audio_path": "assets/ursi.m4a"
+	},
+	"8": {
+		"name": "Pasărea Unicorn",
+		"path": "assets/PASAREA_UNICORN.png",
+		"audio_path": "assets/unicorn.m4a"
+	}
+}
+
+const data_full = {
+	"1": {
+		"name": "Baba Dochia",
+		"path": "assets/BABA_DOCHIA.png"
+	},
+	"2": {
+		"name": "Calul Năzdrăvan",
+		"path": "assets/CALUL_NAZDRAVAN.png"
+	},
+	"3": {
+		"name": "Capra",
+		"path": "assets/CAPRA.png"
+	},
+	"4": {
+		"name": "Cerbul",
+		"path": "assets/CERBUL.png"
+	},
+	"5": {
+		"name": "Drăgaica",
+		"path": "assets/DRAGAICA.png"
+	},
+	"6": {
+		"name": "Greuceanu",
+		"path": "assets/GREUCEANU.png"
+	},
+	"7": {
+		"name": "Luceafărul",
+		"path": "assets/LUCEAFARUL.png"
+	},
+	"8": {
+		"name": "Lupul",
+		"path": "assets/LUPUL.png"
+	},
+	"9": {
+		"name": "Mama Pădurii",
+		"path": "assets/MAMA_PADURII.png"
+	},
+	"10": {
+		"name": "Martolea",
+		"path": "assets/MARTOLEA.png"
+	},
+	"11": {
+		"name": "Miorița",
+		"path": "assets/MIORITA.png"
+	},
+	"12": {
+		"name": "Moș Basca",
+		"path": "assets/MOS_BASCA.png"
+	},
+	"13": {
+		"name": "Pasărea Unicorn",
+		"path": "assets/PASAREA_UNICORN.png"
+	},
+	"14": {
+		"name": "Samca",
+		"path": "assets/SAMCA.png"
+	},
+	"15": {
+		"name": "Solomonar",
+		"path": "assets/SOLOMONAR.png"
+	},
+	"16": {
+		"name": "Ștefan cel Mare",
+		"path": "assets/STEFAN_DEL_MARE.png"
+	},
+	"17": {
+		"name": "Tatăl Pădurii",
+		"path": "assets/TATAL_PADURII.png"
+	},
+	"18": {
+		"name": "Urși",
+		"path": "assets/URSI.png"
+	},
+	"19": {
+		"name": "Verde Împărat",
+		"path": "assets/VERDE_IMPARAT.png"
+	},
+	"20": {
+		"name": "Zburătorul",
+		"path": "assets/ZBURATOR.png"
+	},
+	"21": {
+		"name": "Zmeul",
+		"path": "assets/ZMEUL.png"
+	},
+	"22": {
+		"name": "Zorilă",
+		"path": "assets/ZORILA.png"
+	}
 }
 
 export default data;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import data from './data.js'
 import Logo from './logo.js'
 
+
 let logo = new Logo(document.getElementById("logo"));
 setTimeout( function(){ 
 	logo.move();
 }, 2000);
 
+const infoSection = document.getElementById("info-section");
 const cardsSection = document.getElementById("cards-section");
 setTimeout( function(){
 	cardsSection.style.visibility = "visible";
 	cardsSection.style.opacity = "1";
+	infoSection.style.opacity = "1";
 }, 4000);
+
 
 const parentTbl = document.getElementById("cards-table");
 var cards = []
 var cards_freq = {}
 
+const TOTAL_CARDS = 8;
 function getRandomCards( n ) {
 	for(let i=0; i<n; i++ ) {
-		let card_i = Math.floor(1+Math.random()*22 );
+		let card_i = Math.floor(1+Math.random()*TOTAL_CARDS );
 		while( cards.indexOf(card_i) != -1 ) {
-			card_i = Math.floor(1+Math.random()*22 );
+			card_i = Math.floor(1+Math.random()*TOTAL_CARDS );
 		}
 		cards.push( card_i );
 		cards_freq[card_i] = 2;
 	}
 }
 
-function getRandomCard() {
+function selectRandomCard() {
 	if( cards.length == 0 ) return;
 	let i = Math.floor(Math.random()*(cards.length-1));
 	let card = cards[i];
@@ -43,6 +48,8 @@ function updateTable( n, m ) {
 	
 }
 
+
+const audioPlayer = document.getElementById("card-track");
 function updateCardsState( i, j) {
 	let clickedCard = document.getElementById("backsideCard"+i+'_'+j);
 	if( clickedCard != null) {
@@ -61,6 +68,11 @@ function updateCardsState( i, j) {
 					let c = document.getElementById("imageCard"+i+'_'+j);
 					document.getElementById("card-name").textContent = c.data.name;
 					document.getElementById("card-image").style.backgroundImage = `url('${c.data.path}')`;
+					document.getElementById("intro-text").style.display = "none";
+					document.getElementById("intro-header").style.display = "none";
+					audioPlayer.src = c.data.audio_path;
+					audioPlayer.style.visibility = "visible";
+					audioPlayer.play();
 					c.remove();
 					document.getElementById("imageCard"+l_i+'_'+l_j).remove();
 					initConfetti();	
@@ -72,38 +84,61 @@ function updateCardsState( i, j) {
 		last_j=(last_j!=null)?null:j;
 	}
 }
+function createCard( i, j ) {
+	// Backside Card 
+	const backside = document.createElement("div");
+	backside.id = "backsideCard"+i+'_'+j;
+	backside.className = "backsideCard";
+	backside.addEventListener("click", ()=>{
+		updateCardsState(i, j);
+	});
+
+
+	// Image Card 
+	let c = selectRandomCard();
+	const image = document.createElement("div");
+	image.id = "imageCard"+i+'_'+j;
+	image.style.backgroundImage=`url('${c.path}')`;
+	image.data = c;
+	backside.data = image.style.backgroundImage;
+	image.appendChild( backside );
+
+	// Table Cell
+	const tblCell = document.createElement("td");
+	tblCell.contentEditable="false";
+	tblCell.append( image );
+	return tblCell;
+}
 
 var last_i, last_j;
 function createTable( n, m ) {
 	getRandomCards( n*m/2 );
 	for(let i=0; i<n; i++ ) {
 		const tblRow = document.createElement("tr");
-		for(let j=0; j<m; j++ ) {
-			// Backside Card 
-			const backside = document.createElement("div");
-			backside.id = "backsideCard"+i+'_'+j;
-			backside.className = "backsideCard";
-			backside.addEventListener("click", ()=>{
-				updateCardsState(i, j);
-			});
-
-			// Image Card 
-			let c = getRandomCard();
-			const image = document.createElement("div");
-			image.id = "imageCard"+i+'_'+j;
-			image.style.backgroundImage=`url('${c.path}')`;
-			image.data = c;
-			backside.data = image.style.backgroundImage;
-			image.appendChild( backside );
-
-			// Table Cell
-			const tblCell = document.createElement("td");
-			tblCell.contentEditable="false";
-			tblCell.append( image );
-			tblRow.appendChild(tblCell);
+			
+		// Randomize cell selection
+		let randomIndexes = []
+		let tblCells = []
+		for(let j=0; j<m/2; j++) {
+			let randomIndex = Math.floor(Math.random()*m);
+			while( randomIndexes.indexOf(randomIndex) != -1 ) {
+				randomIndex = Math.floor(Math.random()*m);
+			}
+			randomIndexes.push(randomIndex);	
+			tblCells[randomIndex] = createCard(i, randomIndex);
 		}
+		for(let j=0; j<m; j++ ) {
+			if( randomIndexes.indexOf(j) == -1 )  {
+				tblCells[j] = createCard(i, j);		
+			}
+		}
+
+		for(let j=0; j<m; j++) {
+			tblRow.appendChild(tblCells[j]);
+		}
+
 		parentTbl.appendChild(tblRow);
 	}
 }
 
-createTable(3,4);
+createTable(4,4);
